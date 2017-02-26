ed943ae6e0c472d0fcc57877b91021cc3859f8f2
AtlasQuan <154213158@qq.com>
2017-02-26 13:55:37 +0800

    数据库和addnotice_work的完善
    数据库：增加了表subscribe的主键以及对subscribe的id的类型是notice还是matters的记录
    addnotice_work：增加了对进行addnotice操作的用户的记录

--- AddNotice_work.php
+++ AddNotice_work.php
@@ -16,9 +16,19 @@
 <?php
 	$notice = $_GET['notice'];
 	$notice_id = $_GET['notice_id'];
+	try{
 	$insertnotice=$DBH->prepare("insert into notice (ID,Ndetail) values (?,?)");
 	$insertnotice->bindParam(1,$notice_id);
 	$insertnotice->bindParam(2,$notice);
 	$insertnotice->execute();
-	print_r($insertnotice->errorInfo());
-?>
+	//print_r($insertnotice->errorInfo());
+	$insertsubscribe = $DBH->prepare("insert into subscribe (userID,type,ID) values (?,'notice',?)");
+	$insertsubscribe->bindParam(1,$user_id);
+	$insertsubscribe->bindParam(2,$notice_id);
+	$insertsubscribe->execute();
+	//print_r($insertsubscribe->errorInfo());
+	echo "发布成功！";
+	}
+	catch(PDOException $e){
+		die($e->getMessage());
+	}
